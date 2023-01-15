c7d7e795f7f6bab5c9e2eb96a82b3874107056fa
coleyrockin <coleyrockin@aol.com>
2023-01-15 14:36:19 -0600

    adding routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from app.routes import home
 from flask import Flask
+from app.routes import home, dashboard
 
 
 def create_app(test_config=None):
@@ -17,5 +17,5 @@ def create_app(test_config=None):
 
 # register routes
     app.register_blueprint(home)
-
+    app.register_blueprint(dashboard)
     return app
